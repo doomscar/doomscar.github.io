6b75c942fa46808bdb691770601d8532fea8e66d
doomscar <doomscar@gmail.com>
2018-10-28 21:31:51 +0300

    second_slider

--- slider_js_one/script.js
+++ slider_js_one/script.js
@@ -1,4 +1,8 @@
-slider = function(list, prev, next, currentSlide){
+var container = document.querySelector('.section-slide');
+
+slider(container.querySelectorAll('.slide'), container.querySelector('.prev'), container.querySelector('.next'), 0);
+
+function slider(list, prev, next, currentSlide){
   next.onclick = function() {
     if(currentSlide === list.length-1)
     {
@@ -28,4 +32,25 @@ slider = function(list, prev, next, currentSlide){
   }
 }
 
-slider(document.querySelectorAll('.slide'), document.querySelector('.prev'), document.querySelector('.next'), 0);
+/*############################################ second_slider #########################################*/
+
+carousel(document.querySelector('.second'), 720, 1);
+
+function carousel(section, width, count){
+  section_ul = section.querySelector('.slider');
+  section_li = section_ul.querySelectorAll('.slide');
+
+  var position = 0;
+
+  section.querySelector('.prev').onclick = function(){
+    position = Math.min(position + width * count, 0);
+    section_ul.style.marginLeft = position + 'px';
+  }
+
+  section.querySelector('.next').onclick = function(){
+    position = Math.max(position - width * count, -width * (section_li.length - count));
+    section_ul.style.marginLeft = position + 'px';
+  }
+
+}
+
