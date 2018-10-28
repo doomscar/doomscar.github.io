5535da65ce7eebc44421a04e95287edb5b8b5159
doomscar <doomscar@gmail.com>
2018-10-28 18:32:29 +0300

    2

--- slider_js_one/script.js
+++ slider_js_one/script.js
@@ -1,30 +1,33 @@
 
-var list = document.querySelectorAll('.slide');
-var currentSlide = 0;
-document.querySelector('.next').onclick = function() {
-  if(currentSlide === list.length-1)
-  {
-    list[currentSlide].classList.remove('show');
-    list[0].classList.add('show');
-    currentSlide = 0;
+
+slider = function(list, prev, next, currentSlide){
+  next.onclick = function() {
+    if(currentSlide === list.length-1)
+    {
+      list[currentSlide].classList.remove('show');
+      list[0].classList.add('show');
+      currentSlide = 0;
+    }
+    else
+    {
+      list[currentSlide].classList.remove('show');
+      list[currentSlide+=1].classList.add('show');
+    }
   }
-  else
-  {
-    list[currentSlide].classList.remove('show');
-    list[currentSlide+=1].classList.add('show');
+  
+  prev.onclick = function() {
+    if(currentSlide === 0)
+    {
+      list[currentSlide].classList.remove('show');
+      currentSlide = list.length-1;
+      list[currentSlide].classList.add('show');
+    }
+    else
+    {
+      list[currentSlide].classList.remove('show');
+      list[currentSlide-=1].classList.add('show');
+    }
   }
 }
 
-document.querySelector('.prev').onclick = function() {
-  if(currentSlide === 0)
-  {
-    list[currentSlide].classList.remove('show');
-    currentSlide = list.length-1;
-    list[currentSlide].classList.add('show');
-  }
-  else
-  {
-    list[currentSlide].classList.remove('show');
-    list[currentSlide-=1].classList.add('show');
-  }
-}
+slider(document.querySelectorAll('.slide'), document.querySelector('.prev'), document.querySelector('.next'), 0);
