ca8e209e3edd1131ed7d44cd8c1f4f8f88681a67
doomscar <doomscar@gmail.com>
2018-12-17 00:01:39 +0300

    ++

--- pr-tanks-mod/games/tanks.js
+++ pr-tanks-mod/games/tanks.js
@@ -109,13 +109,13 @@ var game = new Phaser.Game(900, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload () {
 
-    game.load.atlas('tank', '../assets/games/tanks/tanks.png', '../assets/games/tanks/tanks.json');
-    game.load.atlas('enemy', '../assets/games/tanks/enemy-tanks.png', '../assets/games/tanks/tanks.json');
-    game.load.image('logo', '../assets/games/tanks/logo.png');
-    game.load.image('bullet', '../assets/games/tanks/bullet.png');
-    game.load.image('earth', '../assets/games/tanks/scorched_earth.png');
-    game.load.image('brick', '../assets/games/tanks/brick.png');
-    game.load.spritesheet('kaboom', '../assets/games/tanks/explosion.png', 64, 64, 23);
+    game.load.atlas('tank', 'assets/games/tanks/tanks.png', 'assets/games/tanks/tanks.json');
+    game.load.atlas('enemy', 'assets/games/tanks/enemy-tanks.png', 'assets/games/tanks/tanks.json');
+    game.load.image('logo', 'assets/games/tanks/logo.png');
+    game.load.image('bullet', 'assets/games/tanks/bullet.png');
+    game.load.image('earth', 'assets/games/tanks/scorched_earth.png');
+    game.load.image('brick', 'assets/games/tanks/brick.png');
+    game.load.spritesheet('kaboom', 'assets/games/tanks/explosion.png', 64, 64, 23);
     
 }
 
